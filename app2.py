1b9e38534f612c484110d127ce101f6329073922
YASHANS-JOSHI <yashansjoshi@gmail.com>
2025-09-24 18:10:29 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
+
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
@@ -136,4 +137,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
